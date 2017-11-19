3758219f38aab08a215da7aca3ae3db0ee808419
Stas <sromanenko91@gmail.com>
2017-11-20 01:15:30 +0300

    добавил возможность давать свои имена ссылкам, разнес по файликам структуру страницы

--- ShortLink.php
+++ ShortLink.php
@@ -23,7 +23,7 @@ class ShortLink
         return hash('ripemd160', 'mytime'.time() );
     }
     // получаем сокращённую сылку
-    public function getShortLink($longLink)
+    public function getShortLink($longLink, $hash = '')
     {
         $scheme = parse_url($longLink , PHP_URL_SCHEME);
         if (empty($scheme))
@@ -32,33 +32,26 @@ class ShortLink
         $query = 'insert into links values (:hash,:link)';
         $stmt = self::$pdo->prepare($query);
 
-        // если hash уже есть в базе то пробуем 5 раз сформировать другой и записать его.
-        for ($i = 0; $i < 5; $i++)
-        {
-            try{
-                //берём первые 5 символов в hash
+        do {
+            if ($hash == '')
                 $hash = substr(md5('time'.time()), 0, 6 );
-                $sqlAnswer = $stmt->execute([
-                    'hash' => $hash,
-                    'link' => $longLink
-                ]);
-                //если запрос выполнился с ошибкой
-                if ($sqlAnswer == false)
-                    //если ошибка уникальности 'Duplicate entry'
-                    if ($stmt->errorInfo()[1] == 1062)
-                        throw new Exception($stmt->errorInfo()[2]);
+            else
+            {
+                if ($this->isHashInDB($hash))
+                    return 'duplicate';
             }
-            catch(Exception $e) {
+            if ($this->isHashInDB($hash))
                 continue;
-            }
+            $stmt->execute([
+               'hash' => $hash,
+               'link' => $longLink
+            ]);
             break;
-        }
-        if ($i == 5){
-            echo "Не получается создать ссылку, попробуйте позже<br>\n";
-            return null;
-        }
+        } while (true);
+
         return 'http://'.$_SERVER['HTTP_HOST'].'/'.$hash;
     }
+
     // перходим по ссылке если есть соответствие в базе
     public function goToLink($uri)
     {
@@ -67,7 +60,7 @@ class ShortLink
         if (empty($uriArr[1]))
             return 'no link';
 
-        $query = 'select hash, link from links where hash = :hash';
+        $query = 'select link from links where hash = :hash';
         $stmt = self::$pdo->prepare($query);
         $sqlAnswer = $stmt->execute([
             'hash' => $uriArr[1]
@@ -78,5 +71,15 @@ class ShortLink
         $link = $stmt->fetch()['link'];
         header('Location: '. $link);
 
+    }
+    private function isHashInDB($hash)
+    {
+        $query = 'select hash from links where hash = :hash';
+        $stmt = self::$pdo->prepare($query);
+        $stmt->execute(['hash'=> $hash]);
+        $result = $stmt->fetch();
+        if (!empty($result))
+            return true;
+        return false;
     }
 }
--- index.php
+++ index.php
@@ -12,14 +12,22 @@ $redirect = $objLink->goToLink($_SERVER['REQUEST_URI']);
 $token = $objLink->getToken();
 
 //если пришли данные из пост запроса, то сформировать ссылку.
-if (isset($_POST['link'])) {
+if (isset($_POST['link']) && isset($_POST['token'])) {
     if ($_SESSION['token'] === $_POST['token'])
         if (!empty(trim($_POST['link']))) {
             $longLink = $_POST['link'];
             //$longLink = htmlentities($_POST['link']);
-            $shortLink = $objLink->getShortLink($longLink);
+            $shortLink = $objLink->getShortLink($longLink, $_POST['hash']);
         }
 }
 $_SESSION['token'] = $token;
-
-require_once 'view.php';
+if ($redirect === null)
+    echo "Нет соответсвия с короткой ссылкой. Попробуйт создать ещё раз.<br>\n";
+require_once 'view/head.php';
+require_once 'view/body.php';
+if (isset($shortLink))
+    if ($shortLink == 'duplicate')
+        echo "Такое имя ссылки уже занято.<br>\n";
+    else
+        require_once 'view/link.php';
+require_once 'view/footer.php';
